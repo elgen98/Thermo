b1c8900a4d61852f762f60d04da0baae3659ce99
elgen98 <elliot.blomqvist@yahoo.com>
2021-12-02 15:49:20 +0100

    moved models folder to ts folder

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,21 +1,76 @@
-import { Products } from "../Models/models";
+import { Products } from "./Models/models";
 
-let productI = new Products ("Pälsjacka", "", '../assets/image1.png', '../assets/image11.png', false, 2500);
+let productI = new Products(
+  "Pälsjacka",
+  "",
+  "../assets/image1.png",
+  "../assets/image11.png",
+  false,
+  2500
+);
 
-let productII = new Products ("Tjock Dunjacka", "", '../assets/image2.png', '../assets/image22.png', false, 3300);
+let productII = new Products(
+  "Tjock Dunjacka",
+  "",
+  "../assets/image2.png",
+  "../assets/image22.png",
+  false,
+  3300
+);
 
-let productIII = new Products ("Tunn Dunjacka", "", '../assets/image3.png', '../assets/image33.png', false, 2300);
+let productIII = new Products(
+  "Tunn Dunjacka",
+  "",
+  "../assets/image3.png",
+  "../assets/image33.png",
+  false,
+  2300
+);
 
-let productIV = new Products ("Dunis", "", '../assets/image4.png', '../assets/image44.png', false, 4000);
+let productIV = new Products(
+  "Dunis",
+  "",
+  "../assets/image4.png",
+  "../assets/image44.png",
+  false,
+  4000
+);
 
-let productV = new Products ("Silver Dunis", "", '../assets/image5.png', '../assets/image55', false, 3500);
+let productV = new Products(
+  "Silver Dunis",
+  "",
+  "../assets/image5.png",
+  "../assets/image55",
+  false,
+  3500
+);
 
-let productVI = new Products ("Lång Dunster", "", '../assets/image6.png', '../assets/image66.png', false, 4300);
+let productVI = new Products(
+  "Lång Dunster",
+  "",
+  "../assets/image6.png",
+  "../assets/image66.png",
+  false,
+  4300
+);
 
-let productVII = new Products ("Ökenräven", "", '../assets/image7.png', '../assets/image77.png', false, 8000);
+let productVII = new Products(
+  "Ökenräven",
+  "",
+  "../assets/image7.png",
+  "../assets/image77.png",
+  false,
+  8000
+);
 
-let productVIII = new Products ("Skinande Dunis", "", '../assets/image8.png', '../assets/image88.png', false, 10000);
-
+let productVIII = new Products(
+  "Skinande Dunis",
+  "",
+  "../assets/image8.png",
+  "../assets/image88.png",
+  false,
+  10000
+);
 
 var modal = document.getElementById("produktModal") as HTMLDivElement;
 
@@ -23,16 +78,16 @@ var btn = document.getElementById("myBtn") as HTMLButtonElement;
 
 var spanis = document.getElementsByClassName("disappear")[0] as HTMLSpanElement;
 
-btn.onclick = function() {
-    modal.style.display = "block";
-}
+btn.onclick = function () {
+  modal.style.display = "block";
+};
 
-spanis.onclick = function() {
+spanis.onclick = function () {
+  modal.style.display = "none";
+};
+
+window.onclick = function (event) {
+  if (event.target == modal) {
     modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+  }
+};
