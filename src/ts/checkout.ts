461611690d30cfacaacb81538cf12690a6fdb201
elgen98 <elliot.blomqvist@yahoo.com>
2021-12-07 10:08:47 +0100

    ok

---
+++ src/ts/checkout.ts
@@ -0,0 +1,10 @@
+window.onload = function () {
+  document.getElementById("cardType").addEventListener("change", addCardForm);
+};
+
+function addCardForm() {
+  let cardItems = document.getElementsByClassName("cardInfoHidden");
+  for (let i = cardItems.length - 1; i >= 0; --i) {
+    cardItems[i].className = "cardInfoShow";
+  }
+}
