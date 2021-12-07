2ab4bbecf29de7c560e7ccaff4906e2c0ba18193
Sofia Borg <fia.borg@hotmail.com>
2021-12-07 14:12:46 +0100

    infotext added

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -8,73 +8,81 @@ window.onload = function () {
 let productI = new Products(
   "https://img01.ztat.net/article/spp-media-p1/975b832b5da135fa95459afac9e831dd/0497e1047c01452eabdaa5bf73f3ad42.jpg?imwidth=156",
   "https://img01.ztat.net/article/spp-media-p1/7ce6b3c186d73c6c83a3089280c4b4ab/b34654d89ccb47e3ac01747b101e83e0.jpg?imwidth=156&filter=packshot",
-  "Pälsjacka",
+  "Anthony jacket thermo down",
   "",
   2500,
-  false
+  false,
+  "Anthony is a statement jacket cut from a thick down quality. The jacket has an oversized shape - inspired by the traditional aviator style, with a high collar and buckle detailing. The jacket has a hidden button closure and an oversized diagonal pocket in front. Style with Rose pants and a chunky sweater."
 );
 
 let productII = new Products(
   "https://img01.ztat.net/article/spp-media-p1/3c70ab6cbcf84a81aa4d93a0e8d61f5e/931ce05e03954e019b243f60ab5c3397.jpg?imwidth=156",
   "https://img01.ztat.net/article/spp-media-p1/ac203266f7a6444d97a055ee5d275914/25c10042c7d44bca9a4293ca4d32b142.jpg?imwidth=156&filter=packshot",
-  "Tjock Dunjacka",
+  "Sebastian jacket down",
   "",
   3300,
-  false
+  false,
+  "Sebastian is a statement jacket made from down. The jacket has an oversized shape - inspired by the traditional aviator style, with a high collar and buckle detailing. The jacket has a hidden button closure and an oversized diagonal pocket in front. Style with Rose pants and a chunky sweater."
 );
 
 let productIII = new Products(
   "https://img01.ztat.net/article/spp-media-p1/2e2653373d9d4600aca2884b21e1287e/111497f3cba0480290c3bd16ea81c4ab.jpg?imwidth=156",
   "https://img01.ztat.net/article/spp-media-p1/945e8d6415ec4253b36873f9c84c36fd/0af67baf0bbc42b4b7721cbde50c95b0.jpg?imwidth=156&filter=packshot",
-  "Tunn Dunjacka",
+  "Ali jacket down",
   "",
   2300,
-  false
+  false,
+  "Ali is a statement jacket made from down quality. The jacket has an oversized shape - inspired by the traditional aviator style, with a high collar and buckle detailing. The jacket has a hidden button closure and an oversized diagonal pocket in front. Style with Rose pants and a chunky sweater."
 );
 
 let productIV = new Products(
   "https://img01.ztat.net/article/spp-media-p1/4b47edaa76424f2b84c0b9f21c8abb3a/4258bda895964203be8fc47b799ba801.jpg?imwidth=156",
   "https://img01.ztat.net/article/spp-media-p1/1143f8a9a3064e4dae3cee774952e83c/e0800cdfa3b14d459f49032924513199.jpg?imwidth=156&filter=packshot",
-  "Dunis",
+  "Herbert thermo jacket",
   "",
   4000,
-  false
+  false,
+  "Herbert is a jacket made from quality down. The jacket has an oversized shape - inspired by the traditional aviator style, with a high collar and buckle detailing. The jacket has a hidden button closure and an oversized diagonal pocket in front. Style with Rose pants and a chunky sweater."
 );
 
 let productV = new Products(
   "https://img01.ztat.net/article/spp-media-p1/14f120b6e69a43babe509e2aeefd311f/b0fdb212532945049c376b356c3d73e8.jpg?imwidth=156",
   "https://img01.ztat.net/article/spp-media-p1/60bdde7f1afe4ca8adad1851473a1785/6cc8fe9adfa044ea98ad4a7ca83fb72e.jpg?imwidth=156&filter=packshot",
-  "Silver Dunis",
+  "Helin jacket synthetic",
   "",
   3500,
-  false
+  false,
+  "Helin is a statement jacket cut from a wool quality. The jacket has an oversized shape - inspired by the traditional aviator style, with a high collar and buckle detailing. The jacket has a hidden button closure and an oversized diagonal pocket in front. Style with Rose pants and a chunky sweater."
 );
 
 let productVI = new Products(
   "https://img01.ztat.net/article/spp-media-p1/1bd4f7998671401db5ec1be64b6ef189/8e691e6ad22f47848b19a729cb2bf445.jpg?imwidth=156",
   "https://img01.ztat.net/article/spp-media-p1/bf2d293f1ad04090818bd3860dff7f13/fdd5d12c96de4307989eb65199062b3d.jpg?imwidth=156&filter=packshot",
-  "Dunväst",
+  "Ella vest down",
   "",
   4300,
-  false
+  false,
+  "Ella is a down vest cut from a thin wool quality. The jacket has an oversized shape - inspired by the traditional aviator style, with a high collar and buckle detailing. The jacket has a hidden button closure and an oversized diagonal pocket in front. Style with Rose pants and a chunky sweater."
 );
 
 let productVII = new Products(
   "https://img01.ztat.net/article/spp-media-p1/678b276155cd3f8ba6e661b963742508/e9b70c633451466788704ece7f5d2c81.jpg?imwidth=156",
   "https://img01.ztat.net/article/spp-media-p1/275782c86b8d338fa9f8a621bb3585cb/3a5d271abcac477ba9c6018f29122b56.jpg?imwidth=156&filter=packshot",
-  "Ökenräven",
+  "Shima jacket faux fur",
   "",
   8000,
-  false
+  false,
+  "Shima is a statement jacket cut from a thin wool quality. The jacket has an oversized shape - inspired by the traditional aviator style, with a high collar and buckle detailing. The jacket has a hidden button closure and an oversized diagonal pocket in front. Style with Rose pants and a chunky sweater."
 );
 
 let productVIII = new Products(
   "https://img01.ztat.net/article/spp-media-p1/fe881b5c02094390995ce8aaf7c99da3/cbe900b191ed48b5bcd3a2878b8059cb.jpg?imwidth=156",
   "https://img01.ztat.net/article/spp-media-p1/290bb64327674e7c86f18c5512f9b406/853cdb09743443c2b95a70125905416f.jpg?imwidth=156&filter=packshot",
-  "Skinande Dunis",
+  "Hannah jacket down",
   "",
   10000,
-  false
+  false,
+  "Jaqueline is a statement jacket cut from a thin wool quality. The jacket has an oversized shape - inspired by the traditional aviator style, with a high collar and buckle detailing. The jacket has a hidden button closure and an oversized diagonal pocket in front. Style with Rose pants and a chunky sweater."
 );
 
 let productArr = [
@@ -97,7 +105,8 @@ function printProducts() {
     let img: HTMLImageElement = document.createElement("img");
     let img2: HTMLImageElement = document.createElement("img");
     let price: HTMLSpanElement = document.createElement("span");
-    let btn: HTMLButtonElement = document.createElement("button");
+    let btnDiv: HTMLDivElement = document.createElement("div");
+    let infoBtn: HTMLButtonElement = document.createElement("button");
     let addToCartBtn: HTMLButtonElement = document.createElement("button");
 
     addToCartBtn.addEventListener("click", () => {
@@ -111,26 +120,32 @@ function printProducts() {
 
     let productsDiv: HTMLDivElement = document.createElement("div");
 
+    img.src = productArr[i].firstPicture;
+    img2.src = productArr[i].secondPicture;
     name.innerHTML = productArr[i].name;
     size.innerHTML = productArr[i].size;
     img.src = productArr[i].firstPicture;
     img2.src = productArr[i].secondPicture;
-    price.innerHTML = productArr[i].price.toString();
-    addToCartBtn.innerHTML = "ADD TO CART";
+    price.innerHTML = productArr[i].price.toString() + " £";
+    addToCartBtn.innerHTML = "+";
+    infoBtn.innerHTML = "i";
 
-    productsDiv.appendChild(name);
-    productsDiv.appendChild(size);
     productsDiv.appendChild(img);
     productsDiv.appendChild(img2);
+    productsDiv.appendChild(name);
+    productsDiv.appendChild(size);
     productsDiv.appendChild(price);
-    productsDiv.appendChild(btn);
-    productsDiv.appendChild(addToCartBtn);
+    btnDiv.appendChild(infoBtn);
+    btnDiv.appendChild(addToCartBtn);
+    productsDiv.appendChild(btnDiv);
     productContainer.appendChild(productsDiv);
 
-    btn.className = "btnStyle";
+    productContainer.className = "productContainerStyle";
+    btnDiv.className = "btnDivStyle";
+    infoBtn.className = "btnStyle";
+    addToCartBtn.className = "cartBtnStyle";
     img.className = "firstImg";
     img2.className = "secondImg";
-    productContainer.className = "productContainerStyle";
 
     img.style.display = "block";
     img2.style.display = "none";
@@ -145,7 +160,7 @@ function printProducts() {
       img2.style.display = "none";
     };
 
-    btn.addEventListener("click", () => {
+    infoBtn.addEventListener("click", () => {
       detailClick(i);
     });
   }
@@ -155,7 +170,8 @@ function detailClick(position) {
   let name: HTMLHeadingElement = document.createElement("h2");
   let size: HTMLSpanElement = document.createElement("span");
   let img2: HTMLImageElement = document.createElement("img");
-  let price: HTMLSpanElement = document.createElement("span");
+  let price: HTMLSpanElement = document.createElement("p");
+  let infotext: HTMLSpanElement = document.createElement("span");
   let detailDiv: HTMLDivElement = document.createElement("div");
   let detailContainer: HTMLDivElement = document.getElementById(
     "cart-container"
@@ -165,12 +181,14 @@ function detailClick(position) {
   name.innerHTML = productArr[position].name;
   size.innerHTML = productArr[position].size;
   img2.src = productArr[position].secondPicture;
-  price.innerHTML = productArr[position].price.toString();
+  infotext.innerHTML = productArr[position].detailText;
+  price.innerHTML = productArr[position].price.toString() + " £";
 
+  detailDiv.appendChild(img2);
   detailDiv.appendChild(name);
   detailDiv.appendChild(size);
-  detailDiv.appendChild(img2);
   detailDiv.appendChild(price);
+  detailDiv.appendChild(infotext);
   detailContainer.appendChild(detailDiv);
 
   handleClick();
