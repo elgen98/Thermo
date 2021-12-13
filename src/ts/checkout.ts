666ac502bbba4b327e588bc2b453875ab1081106
Sofia Borg <fia.borg@hotmail.com>
2021-12-13 09:46:16 +0100

    added localstorage to checkout summary

--- src/ts/checkout.ts
+++ src/ts/checkout.ts
@@ -1,6 +1,7 @@
 window.onload = function () {
   document.getElementById("cardType").addEventListener("change", addCardForm);
   document.getElementById("confirmorderBtn").addEventListener("click", confirm);
+  printsummary();
 };
 
 function addCardForm() {
@@ -18,16 +19,48 @@ function confirm() {
   showModal();
 }
 
+let retrievedProducts = JSON.parse(localStorage.getItem("product"));
+console.log(retrievedProducts);
 function printsummary() {
-  let product = document.getElementById("products") as HTMLTableColElement;
-  let qty = document.getElementById("qty") as HTMLTableColElement;
-  let price = document.getElementById("price") as HTMLTableColElement;
+  for (let i: number = 0; i < retrievedProducts.length; i++) {
+    if (retrievedProducts[i].cart === true) {
+      let table = document.getElementById("table") as HTMLTableSectionElement;
+      let tr = document.createElement("tr") as HTMLTableRowElement;
+      let dataName = document.createElement("td") as HTMLTableCellElement;
+      let dataQty = document.createElement("td") as HTMLTableCellElement;
+      let dataPrice = document.createElement("td") as HTMLTableCellElement;
+      let dataSize = document.createElement("td") as HTMLTableCellElement;
+
+      dataName.className = "td";
+      dataQty.className = "td";
+      dataSize.className = "td";
+      dataName.className = "td";
+
+      dataName.innerHTML += retrievedProducts[i].name;
+      dataQty.innerHTML += retrievedProducts[i].quantity;
+      dataPrice.innerHTML += retrievedProducts[i].price;
+      dataSize.innerHTML += retrievedProducts[i].size;
+
+      tr.appendChild(dataName);
+      tr.appendChild(dataQty);
+      tr.appendChild(dataPrice);
+      tr.appendChild(dataSize);
+      table.appendChild(tr);
+    }
+  }
+  printTotal();
 }
 
-//skapar en funktion som hämtar lagrade produktera
-function fromLocalStorage() {
-  let retrievedProducts: string = localStorage.getItem(product);
-  JSON.parse(retrievedProducts);
+function printTotal() {
+  let totalAmount: number = 0;
+  let amountDiv = document.getElementById("amountDiv") as HTMLDivElement;
+  for (let i: number = 0; i < retrievedProducts.length; i++) {
+    if (retrievedProducts[i].cart === true) {
+      totalAmount += retrievedProducts[i].totalPrice;
+
+      amountDiv.innerHTML = totalAmount.toString();
+    }
+  }
 }
 
 let modal = document.getElementById("checkoutModal") as HTMLDivElement;
